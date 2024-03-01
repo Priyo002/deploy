f9257029f2511f422ca4c6cc8600ef2cc66a4eda
Priyabrata Das <priyobapi@gmail.com>
2024-03-01 11:16:33 +0530

    action added6

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.send("Hello World and it is working3");
+    res.send("Hello World and it is working10");
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'));
